91d228b65e88f321223e3e1e341c0ea67bd391b6
akashbijukumar <akash.bijukumar123@gmail.com>
2026-01-16 02:50:39 +0530

    Email4

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,17 +18,10 @@ const Contact = () => {
 
         formData.append("access_key", "32335743-4ac7-4a41-af41-ff07ea6cc156");
 
-        const object = Object.fromEntries(formData);
-        const json = JSON.stringify(object);
-
         try {
             const res = await fetch("https://api.web3forms.com/submit", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json"
-                },
-                body: json
+                body: formData
             }).then((res) => res.json());
 
             if (res.success) {
